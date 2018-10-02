301b8113ebd56ed1308068eee70a61aacfeb765b
Ankush Thakur <ankushthakur12@gmail.com>
2018-10-02 14:42:51 -0400

    some minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ParallaxDirective } from './parallax.directive';
 import { AgmCoreModule } from '@agm/core';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     ParallaxDirective,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
