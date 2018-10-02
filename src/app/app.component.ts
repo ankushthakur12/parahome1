bd9ee5e6150de0676821cf05332a395fa719121e
Ankush Thakur <ankushthakur12@gmail.com>
2018-10-02 14:53:56 -0400

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
-import {} from '@googlemaps';
+import { } from '@googlemaps';
 
 
 import { ViewChild, ElementRef, NgZone } from '@angular/core';
@@ -16,23 +16,23 @@ export class AppComponent {
 
   @ViewChild('search') public searchElement: ElementRef;
 
-  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {}
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
 
   ngOnInit() {
-this.mapsAPILoader.load().then(
-() => {
-let autocomplete = new google.maps.places.Autocomplete(this.searchElement.nativeElement, { types:["address"] });
+    this.mapsAPILoader.load().then(
+      () => {
+        let autocomplete = new google.maps.places.Autocomplete(this.searchElement.nativeElement, { types: ["address"] });
 
-autocomplete.addListener("place_changed", () => {
-this.ngZone.run(() => {
-let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-if(place.geometry === undefined || place.geometry === null ){
- return;
-}
-});
-});
-}
-);
+        autocomplete.addListener("place_changed", () => {
+          this.ngZone.run(() => {
+            let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+            if (place.geometry === undefined || place.geometry === null) {
+              return;
+            }
+          });
+        });
+      }
+    );
   }
 
   title = 'parahome';
